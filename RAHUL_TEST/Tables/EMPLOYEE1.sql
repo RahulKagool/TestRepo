0b454a3da62b79e10cc36e0b981e6089640148ae
RahulKagool <39952671+RahulKagool@users.noreply.github.com>
2018-06-06 15:50:42 +0530

    request to merge into master

---
+++ RAHUL_TEST/Tables/EMPLOYEE1.sql
@@ -0,0 +1,4 @@
+CREATE TABLE rahul_test.employee1 (
+  "NAME" VARCHAR2(100 BYTE),
+  "ID" NUMBER(10)
+);
--- RAHUL_TEST/Tables/STUDENT.sql
+++ RAHUL_TEST/Tables/STUDENT.sql
@@ -1,5 +1,6 @@
 CREATE TABLE rahul_test.student (
   "NAME" VARCHAR2(100 BYTE),
   rollno NUMBER(3),
-  subject VARCHAR2(100 BYTE)
+  subject VARCHAR2(100 BYTE),
+  names VARCHAR2(100 BYTE)
 );
