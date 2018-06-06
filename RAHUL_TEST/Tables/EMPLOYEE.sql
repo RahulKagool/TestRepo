e9fe7dd3f6fbb93bea4785ff65276965b6ecca47
RahulKagool <39952671+RahulKagool@users.noreply.github.com>
2018-06-06 18:42:57 +0530

    version 3.0

--- RAHUL_TEST/Tables/EMPLOYEE.sql
+++ RAHUL_TEST/Tables/EMPLOYEE.sql
@@ -3,5 +3,6 @@ CREATE TABLE rahul_test.employee (
   "ID" NUMBER(3),
   city VARCHAR2(100 BYTE),
   subject VARCHAR2(100 BYTE),
-  dept VARCHAR2(100 BYTE)
+  dept VARCHAR2(100 BYTE),
+  names VARCHAR2(255 BYTE)
 );
